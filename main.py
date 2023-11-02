0e95f4967da2f579388548cd3f372e01633c8251
Yeicor <4929005+Yeicor@users.noreply.github.com>
2023-11-02 21:12:45 +0100

    Design clip improvements and complete draft drawings

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import cadquery as cq
 from cq_warehouse.drafting import Draft
 
@@ -9,16 +11,18 @@ wall = wall_min * 3  # Recommended width for most walls of this print
 eps = 1e-5  # A small number
 
 # Bike measurements
-stem_screw_diameter = 4.0  # Diameter of the screw that connects the stem to the fork
+stem_screw_radius = 2.0  # Diameter of the screw that connects the stem to the fork
 stem_circle_flat_radius = 10.0  # Radius of the flat part of the circle
-stem_circle_radius = 15  # Radius of the circle
+stem_circle_radius = 16  # Radius of the circle
 stem_circle_max_height = 3.0  # Maximum height of the circle
 stem_rect = (35, 35)  # Stem "rectangle"
 stem_range = (20, 45)  # Where to connect to the stem from the center of the circle
 stem_fillet = 5.0  # Fillet radius of the stem (square -> circle)
+stem_clip = (stem_fillet, 2.0)  # How much to overlap the stem with our model for a stronger connection
 
 # Create drawing instance with appropriate settings
-draft = Draft(font_size=3)
+draft = Draft(font_size=2.5)
+draft_z = Draft(font_size=2.5, label_normal=(1, 0, 0))
 
 # ================== MODELLING ==================
 
@@ -37,7 +41,7 @@ obj = (
     .extrude(wall)
     .moveTo(0, 0)
     # Remove the screw hole
-    .circle(stem_screw_diameter / 2)  # To remove the screw hole
+    .circle(stem_screw_radius)  # To remove the screw hole
     .cutThruAll()
 )
 
@@ -49,6 +53,7 @@ stem_circle_forbidden = (
 )
 obj += stem_circle_forbidden.faces("<Z").shell(wall)
 obj -= stem_circle_forbidden
+stem_circle_forbidden = None
 
 # Add stem rect limits
 obj += (
@@ -57,18 +62,74 @@ obj += (
     .extrude(-wall)
     .pushPoints([(0, (stem_rect[1] + wall) / 2), (0, -(stem_rect[1] + wall) / 2)])
     .rect(-(stem_range[1] - stem_range[0]), wall, centered=(False, True))
-    .extrude(-stem_rect[0])
-    # Inner cantilever
-    .faces("<Z")
-    .each(lambda f: cq.Workplane(f).rect((stem_range[1] - stem_range[0]),
-                                         (wall/1.999 + stem_fillet) * (1 if f.BoundingBox().ymin < 0 else -1),
-                                         centered=(True, False)).extrude(wall).val())
+    .extrude(-(stem_rect[0] + wall * 2))
 )
+# Inner cantilever
+for f in obj.faces("|Y").faces(">>Y[1] or <<Y[1]").faces(">X").vals():
+    bb = f.BoundingBox()
+    newObj = (
+        cq.Workplane("XZ", (bb.xmin, bb.ymin, bb.zmin))
+        .rect(stem_range[1] - stem_range[0], stem_clip[0], centered=False)
+        .extrude(stem_clip[1] * (-1 if bb.ymin < 0 else 1))
+        .faces(">Y" if bb.ymin < 0 else "<Y").edges("|X"))
+    newObj = newObj.chamfer(stem_clip[1] - eps, stem_clip[0] / 4)
+    newObj = newObj.edges(">Y" if bb.ymin < 0 else "<Y").edges("|X").fillet(stem_clip[0] / 4 - eps)
+    obj += newObj
+    newObj = None
+
 # Stem fillet
 obj = (
-    obj.faces("|Y").faces(">>Y[1] or <<Y[1]").faces(">X").edges(">Z or <Z").fillet(stem_fillet)
+    obj.faces("|Y").faces(">>Y[1] or <<Y[1]").faces(">X").edges(">Z").fillet(stem_fillet)
     .edges(">Y or <Y").edges(">Z or <Z").fillet(wall)
 )
 
-#
-show_object(obj)
+# Show main parameters in the model (for debugging, as it reduces performance)
+if os.getenv('cq_draft'):
+    drafts = cq.Assembly(None)
+    draft_dist = 3 * wall
+    draft_line = draft.font_size * 1.5
+    drafts.add(draft.extension_line([(-stem_screw_radius, 0, 0), (0, 0, 0)], stem_rect[1] / 2 + draft_dist,
+                                    "stem_screw_radius"))
+    drafts.add(draft.extension_line([(-stem_circle_flat_radius, 0, 0), (0, 0, 0)],
+                                    stem_rect[1] / 2 + draft_dist + draft_line, "stem_circle_flat_radius"))
+    drafts.add(draft.extension_line([(-stem_circle_radius, 0, 0), (0, 0, 0)],
+                                    stem_rect[1] / 2 + draft_dist + draft_line * 2, "stem_circle_radius"))
+    drafts.add(
+        draft_z.extension_line([(-stem_circle_radius, 0, 0), (-stem_circle_radius, 0, stem_circle_max_height)],
+                               draft_dist, "stem_circle_max_height"))
+    drafts.add(
+        draft.extension_line([(-stem_circle_radius - wall, 0, 0), (-stem_circle_radius, 0, 0)],
+                             stem_rect[1] / 2 + draft_dist + draft_line * 3, "wall"))
+    drafts.add(
+        draft.extension_line([(0, 0, 0), (stem_range[0], 0, 0)],
+                             -(stem_rect[1] / 2 + draft_dist),
+                             "stem_range[0]"))
+    drafts.add(
+        draft.extension_line([(0, 0, 0), (stem_range[1], 0, 0)],
+                             -(stem_rect[1] / 2 + draft_dist + draft_line),
+                             "stem_range[1]"))
+    drafts.add(
+        draft_z.extension_line([(stem_range[1], -stem_rect[1] / 2, 0), (stem_range[1], stem_rect[1] / 2, 0)],
+                               draft_dist, "stem_rect[1]"))
+    drafts.add(
+        draft_z.extension_line(
+            obj.faces(">X").edges("not |Y").edges("<<Z[1]").val(),
+            draft_dist + draft_line, "stem_fillet"))
+    drafts.add(
+        draft_z.extension_line(
+            [(stem_range[1], -stem_rect[1] / 2, 0), (stem_range[1], -stem_rect[1] / 2, -stem_rect[0])],
+            -draft_dist, "stem_rect[0]"))
+    drafts.add(
+        draft_z.extension_line(
+            [(stem_range[1], -stem_rect[1] / 2, -stem_rect[0] - wall + stem_clip[0]),
+             (stem_range[1], -stem_rect[1] / 2, -stem_rect[0] - wall)],
+            -draft_dist, "stem_clip[0]"))
+    drafts.add(
+        draft.extension_line(
+            [(stem_range[1], -stem_rect[1] / 2, -stem_rect[0] - wall + stem_clip[0] / 2),
+             (stem_range[1], -stem_rect[1] / 2 + stem_clip[1], -stem_rect[0] - wall + stem_clip[0] / 2)],
+            -draft_dist, "stem_clip[1]"))
+    show_object(drafts, "drafts")
+
+    # Show the model
+    show_object(obj, "obj")
